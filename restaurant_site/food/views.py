5b4528cf62b780f8acd6bb48cbc283bb31a71e28
ChinmayKumarSahoo77 <chinmayasahoo9777@gmail.com>
2023-10-28 21:48:36 +0530

    Change in welcome message

--- restaurant_site/food/views.py
+++ restaurant_site/food/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 def greet(request):
     # print("Welcome to our website")
-    return HttpResponse("Welcome to our website...")
+    return HttpResponse("Welcome to our restaurant website...")
