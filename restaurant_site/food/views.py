8274eddeaba3b20538700b5bf748e6be850e9a63
chinmaya.sahoo1 <chinmayasahoo9777@gmail.com>
2023-11-01 23:53:30 +0530

    Added templates

--- restaurant_site/food/views.py
+++ restaurant_site/food/views.py
@@ -1,10 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
+from .models import Item
 
 # Create your views here.
+
+"""
+#Render Using HTTPResponse 
 def greet(request):
-    return HttpResponse("Welcome to our restaurant website...")
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'itemList': item_list,
+    }
+    return HttpResponse(template.render(context,request))
+"""
+
+def greet(request):
+    """
+    Render template using django render
+    """
+    item_list = Item.objects.all()
+    # template = loader.get_template('food/index.html')
+    context = {
+        'itemList': item_list,
+    }
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse("<h1>Here is the item list view</h1>")
