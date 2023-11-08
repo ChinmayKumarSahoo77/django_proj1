11430466ab1cdbc771e0aba0612b0e94a1faf1c1
chinmaya.sahoo1 <chinmayasahoo9777@gmail.com>
2023-11-08 22:34:04 +0530

    In successful creation of user showed the alert message using base.html file and alert class of bootstrap

--- restaurant_site/users/views.py
+++ restaurant_site/users/views.py
@@ -1,8 +1,19 @@
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    if request.method == "POST":
+        form_data = UserCreationForm(request.POST) # retriving the form data
+        if form_data.is_valid(): # validate form data
+            user_name = form_data.cleaned_data.get('username') # get username from form_data
+            # using message.success() trying to print the message
+            messages.success(request, f'Welcome {user_name}, your account is created successfully.')
+            return redirect('food:greet') #render list of food page
+        
+    else:
+        # create the form if method is not POST
+        form_data = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form_data})
