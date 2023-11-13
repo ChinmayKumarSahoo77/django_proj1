d0aea58fa47bf66bf2ac04ac741ba926f8e61cb5
chinmaykumarsahoo77 <chinmayasahoo9777@gmail.com>
2023-11-13 11:19:46 +0530

    Added user profile

--- restaurant_site/users/models.py
+++ restaurant_site/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # It creates one to one relation with User model
+    # on_delete=models.CASCADE - On deleting user the profile also need to be deleted
+    
+    image = models.ImageField(default="profile_pic.jpg", upload_to="profile_pictures")
+    locations = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.user.username
