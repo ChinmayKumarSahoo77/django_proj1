05bea214ecdcce0a6133e1a1ccfb552bf2c570ab
chinmaykumarsahoo77 <chinmayasahoo9777@gmail.com>
2023-11-15 23:26:33 +0530

    Adding profile pic/static files to profile page or any web page

--- restaurant_site/restaurant_site/urls.py
+++ restaurant_site/restaurant_site/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_view
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from users import views as user_view
 
 urlpatterns = [
@@ -30,3 +33,10 @@ urlpatterns = [
     # template_name = "users/login.html" -> tell django to look for template in specified location
     path('profile/', user_view.profile_page, name='profile'),
 ]
+
+# Search how to manage static files in django
+# Copy the code from following section and add it to urls.py of main project:
+# Serving static files during development
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
